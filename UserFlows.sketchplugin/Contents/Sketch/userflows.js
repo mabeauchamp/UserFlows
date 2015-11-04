cf5021aa9d0a93f9237da51f4f8b015361c1f09e
Aby N <aby@scooterinteractive.com>
2015-11-04 23:00:32 +0800

    Export Flow diagrams from top to bottom
    - Updated Settings Dialog to include the Horizontal vs Vertical option
    (Defaults to horizontal)
    - Updated flow exporter to export flows vertically based on the setting

--- UserFlows.sketchplugin/Contents/Sketch/userflows.js
+++ UserFlows.sketchplugin/Contents/Sketch/userflows.js
@@ -8,10 +8,12 @@ var userDefaults = {
 	showModifiedDate:1,
 	minimumTapArea:44.0,
 	fullName:"",
-	exportFormat:"PDF"
+	exportFormat:"PNG",
+	flowDirection:"H"
 }
 
 var scaleOptions = ['1x', '2x'];
+var flowDirections = ['Horizontal', 'Vertical']
 var formatOptions = [NSArray arrayWithObjects:"PNG", "JPG", "TIFF", nil]
 iconName = "icon.png"
 
@@ -109,12 +111,13 @@ var generateFlowWithSettings = function(s) {
 	
 	var exportScale = s.exportToScale,
 		exportFormat = getDefault('exportFormat').toLowerCase(),
+		flowVertical = getDefault('flowDirection') == 'V',
 		spacing = 50*exportScale,
 		outerPadding = 40*exportScale,
 		selectedArtboards = [NSArray array],
 		flowBoard = [MSArtboardGroup new],
-		flowWidth = outerPadding, 
-		flowHeight = 0,
+		flowWidth = flowVertical ? 0 : outerPadding, 
+		flowHeight = flowVertical ? outerPadding : 0,
 		flowName = s.flowName,
 		flowGroup = addGroup(flowName, flowBoard),
 		flowDescription = s.flowDescription,
@@ -177,7 +180,7 @@ var generateFlowWithSettings = function(s) {
 	setSize(flowLabel, 10, 10)
 	
 	// create images for artboards and populate the flow artboard
-	var artboard, screenImage, screenLayer, screenFrame, aWidth, aHeight, selectionRect, hitAreaLayer, hitAreaFrame, arrowContainer, textLayer, textFrame, screenContainer, screenY, arrow, arrowStartPoint, arrowEndPoint, arrowY, screenNumber = 0;
+	var artboard, screenImage, screenLayer, screenFrame, aWidth, aHeight, selectionRect, hitAreaLayer, hitAreaFrame, arrowContainer, textLayer, textFrame, screenContainer, screenY, arrow, arrowStartPoint, arrowEndPoint, arrowY, arrowX, screenNumber = 0;
 	
 	loop = [selectedArtboards objectEnumerator]
 	while (ar = [loop nextObject]) {
@@ -188,7 +191,11 @@ var generateFlowWithSettings = function(s) {
 		aHeight = screenFrame.height*exportScale
 		
 		screenContainer = addGroup([artboard name], flowGroup)
-		setPosition(screenContainer, flowWidth, outerPadding)
+		if(flowVertical) {
+			setPosition(screenContainer, outerPadding, flowHeight)
+		} else {
+			setPosition(screenContainer, flowWidth, outerPadding)
+		}
 		
 		screenNumber++;
 		textLayer = addText([artboard name], screenContainer, 12*exportScale)
@@ -209,15 +216,22 @@ var generateFlowWithSettings = function(s) {
 		if(selectionRect.width != 0 && selectionRect.height != 0) {
 			arrowContainer = addGroup("Flow Indicator", screenContainer)
 			hitAreaLayer = addShape("Tap Area", arrowContainer)
-			setPosition(hitAreaLayer, flowFrame.x+flowWidth+(selectionRect.x*exportScale), flowFrame.y+outerPadding+screenY+(selectionRect.y*exportScale), true)
+			
+			if(flowVertical) {
+				setPosition(hitAreaLayer, flowFrame.x+outerPadding+(selectionRect.x*exportScale), flowFrame.y+flowHeight+screenY+(selectionRect.y*exportScale), true)
+			} else {
+				setPosition(hitAreaLayer, flowFrame.x+flowWidth+(selectionRect.x*exportScale), flowFrame.y+outerPadding+screenY+(selectionRect.y*exportScale), true)
+			}
+
 			setSize(hitAreaLayer, (selectionRect.width*exportScale), (selectionRect.height*exportScale), true)
 			setColor(hitAreaLayer, '000000', 0)
 			setBorder(hitAreaLayer, 2*exportScale, 2, flowIndicatorColor, 1)
 		
 			hitAreaFrame = getFrame(hitAreaLayer)
-			arrowY = hitAreaFrame.y+(hitAreaFrame.height/2);
-			arrowStartPoint = {x:hitAreaFrame.x+hitAreaFrame.width, y:arrowY}
-			arrowEndPoint = {x:aWidth+(spacing/2), y:arrowY}
+			arrowY = flowVertical ? hitAreaFrame.y+hitAreaFrame.height : hitAreaFrame.y+(hitAreaFrame.height/2);
+			arrowX = flowVertical ? hitAreaFrame.x+(hitAreaFrame.width/2) : hitAreaFrame.x+hitAreaFrame.width
+			arrowStartPoint = {x:arrowX, y:arrowY}
+			arrowEndPoint = flowVertical ? {x:arrowX, y:screenY+aHeight+(spacing/2)} : {x:aWidth+(spacing/2), y:arrowY}
 			arrow = addLine('Flow Arrow', arrowContainer, arrowStartPoint, arrowEndPoint, 2*exportScale, flowIndicatorColor)
 
 			[[arrow firstLayer] setEndDecorationType:1]
@@ -226,16 +240,26 @@ var generateFlowWithSettings = function(s) {
 		}
 	
 		[screenContainer resizeRoot:false];
-		flowWidth += getFrame(screenContainer).width + spacing
-		flowHeight = Math.max(flowHeight, getFrame(screenContainer).height)
+		if(flowVertical) {
+			flowWidth = Math.max(flowWidth, getFrame(screenContainer).width)
+			flowHeight += getFrame(screenContainer).height + spacing
+		} else {
+			flowWidth += getFrame(screenContainer).width + spacing
+			flowHeight = Math.max(flowHeight, getFrame(screenContainer).height)
+		}
 	}
 
 	// update flow artboard dimensions and add it to current page
 	[flowGroup resizeRoot:false];
 	[flowGroup setHasClickThrough:true];
 	
-	flowWidth -= (spacing-outerPadding)
-	flowHeight += (outerPadding*2);
+	if(flowVertical) {
+		flowHeight -= (spacing-outerPadding)
+		flowWidth += (outerPadding*2);
+	} else {
+		flowWidth -= (spacing-outerPadding)
+		flowHeight += (outerPadding*2);
+	}
 	
 	setSize(flowLabel, flowWidth-(outerPadding*2), 10);
 	[flowLabel setTextBehaviour:1];
@@ -266,7 +290,11 @@ var generateFlowWithSettings = function(s) {
 	var flowInfoHeight = (s.showModifiedDate == 1) ? flowLabelFrame.height + 14 + descriptionLabelHeight + getFrame(modifiedDateLabel).height : flowLabelFrame.height + 14 + descriptionLabelHeight;
 	
 	setPosition(flowGroup, flowLabelFrame.x, flowInfoHeight+(outerPadding*2))
-	setSize(flowBoard, flowWidth, flowHeight+flowInfoHeight+outerPadding)
+	// if (flowVertical) {
+	// 	setSize(flowBoard, flowWidth, flowHeight+flowInfoHeight+(outerPadding*2))
+	// } else {
+		setSize(flowBoard, flowWidth, flowHeight+flowInfoHeight+outerPadding)
+	// }
 
 	[flowGroup resizeRoot:false];
 	
@@ -363,31 +391,42 @@ var showSettingsDialog = function() {
 	[alert addTextLabelWithValue: 'Minimum Tap Area'] // 5
 	[alert addAccessoryView: createTextArea(getDefault('minimumTapArea')+"px", 50, 23)] // 6
 	
-	[alert addAccessoryView: createSeparator()] // 7
+	var lastSavedFlowDirection = getDefault('flowDirection')
+	var fdIndex = lastSavedFlowDirection == 'H' ? 0 : 1
+	var flowDirectionRadioButtons = createRadioButtons(flowDirections, 1, flowDirections.length, "Flow Direction", fdIndex)
+
+	[alert addTextLabelWithValue: 'Flow Direction'] // 7
+	[alert addAccessoryView: flowDirectionRadioButtons] // 8
+
+	[alert addAccessoryView: createSeparator()] // 9
 	
-	[alert addTextLabelWithValue: 'Your Name'] // 8
-	[alert addTextFieldWithValue: getDefault('fullName')] // 9
+	[alert addTextLabelWithValue: 'Your Name'] // 10
+	[alert addTextFieldWithValue: getDefault('fullName')] // 11
 	
-	[alert addAccessoryView: createCheckbox({name: 'Show Date and Name on Flows', value: 'showModifiedDate'}, getDefault('showModifiedDate'))] // 10
+	[alert addAccessoryView: createCheckbox({name: 'Show Date and Name on Flows', value: 'showModifiedDate'}, getDefault('showModifiedDate'))] // 12
 	
-	[alert addAccessoryView: createSeparator()] // 11
+	[alert addAccessoryView: createSeparator()] // 13
 	
 	[alert addButtonWithTitle: 'Reset Defaults'];
 	
 	var response = [alert runModal]
 	
 	if (response == "1000") {
-		scaleIndex = [[radioButtons selectedCell] tag]-100;
+		var scaleIndex = [[radioButtons selectedCell] tag]-100;
+
+		fdIndex = [[flowDirectionRadioButtons selectedCell] tag]-100;
+		var fDirection = fdIndex == 0 ? 'H' : 'V'
 
 
 		var newColor = [colorWell color],
 			exportFormat = [formatDropdown titleOfSelectedItem],
 			newHex = NSColorToHex(newColor)
 		setDefault('exportScaleIndex', scaleIndex)
+		setDefault('flowDirection', fDirection)
 		setDefault('flowIndicatorColor', newHex)
 		setDefault('minimumTapArea', parseFloat(valueAtIndex(alert, 6)))
-		setDefault('fullName', valueAtIndex(alert, 9))
-		setDefault('showModifiedDate', checkedAtIndex(alert, 10))
+		setDefault('fullName', valueAtIndex(alert, 11))
+		setDefault('showModifiedDate', checkedAtIndex(alert, 12))
 		setDefault('exportFormat', exportFormat)
 	} else if (response == "1002") {
 		resetDefaults(userDefaults)
